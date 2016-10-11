4dcbc9966327a25615e95e1205fed805af66ef46
🤔ohju <ohju.lim@gmail.com>
2016-10-11 16:43:35 +0900

    fixed the first bug

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 	
-	var currentValue: Int = 0
+	var currentValue: Int = 50
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
 	@IBAction func showAlert() {
 		let message = "The value of the slider is: \(currentValue)"
 		let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
-		let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+		let action = UIAlertAction(title: "OK", style: .default, handler: nil)
 		alert.addAction(action)
 		present(alert, animated: true, completion: nil)
 	}
