2c5daadd2a0d5672bf14fca876a7370dd9c3168a
🤔ohju <ohju.lim@gmail.com>
2016-10-11 17:11:46 +0900

    Generate a random number on Pop-up.
    Explain the instance variable once again.

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 	
 	var currentValue: Int = 0
-	
 	@IBOutlet weak var slider: UISlider!
+	var targetValue: Int = 0
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		currentValue = lroundf(slider.value)
+		targetValue = 1 + Int(arc4random_uniform(100))
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -25,7 +26,7 @@ class ViewController: UIViewController {
 	}
 	
 	@IBAction func showAlert() {
-		let message = "The value of the slider is: \(currentValue)"
+		let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)"
 		let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
 		let action = UIAlertAction(title: "OK", style: .default, handler: nil)
 		alert.addAction(action)
