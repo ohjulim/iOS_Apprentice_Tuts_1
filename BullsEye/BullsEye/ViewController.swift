37c2c3e698a7c4b95080c04b6abd27b1b772d41e
🤔ohju <ohju.lim@gmail.com>
2016-10-13 16:37:44 +0900

    improve algorithm 1

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -37,13 +37,9 @@ class ViewController: UIViewController {
 	}
 	
 	@IBAction func showAlert() {
-		var difference: Int
-		if currentValue > targetValue {
-			difference = currentValue - targetValue
-		} else if targetValue > currentValue {
-			difference = targetValue - currentValue
-		} else {
-			difference = 0
+		var difference = currentValue - targetValue
+		if difference < 0 {
+			difference = difference * -1
 		}
 		let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\nThe difference is: \(difference)"
 		let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
