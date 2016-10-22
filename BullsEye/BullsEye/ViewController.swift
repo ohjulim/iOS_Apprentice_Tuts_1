c42157027f0118952f9038133c59d58b985c162b
🤔ohju <ohju.lim@gmail.com>
2016-10-23 02:43:33 +0900

    Crossfade

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
 	
@@ -98,6 +99,13 @@ class ViewController: UIViewController {
 	@IBAction func startOver(_ sender: UIButton) {
 		startNewGame()
 		updateLabels()
+		
+		// Crossfade
+		let transition = CATransition()
+		transition.type = kCATransitionFade
+		transition.duration = 1
+		transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+		view.layer.add(transition, forKey: nil)
 	}
 
 
