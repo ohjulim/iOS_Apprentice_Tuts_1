32daa99634eecd1be6e681b0362b0cb60bb0460b
🤔ohju <ohju.lim@gmail.com>
2016-10-23 00:46:54 +0900

    Add HTML file on info screen

--- BullsEye/BullsEye/AboutViewController.swift
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class AboutViewController: UIViewController {
 	
+	@IBOutlet weak var webView: UIWebView!
+	
 	@IBAction func close() {
 		dismiss(animated: true, completion: nil)
 	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
+			
+			// This loads the local HTML file into the web view.
+			if let url = Bundle.main.url(forResource: "BullsEye", withExtension: "html") {
+				if let htmlData = try? Data(contentsOf: url) {
+					let baseURL = URL(fileURLWithPath: Bundle.main.bundlePath)
+					webView.load(htmlData, mimeType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+				}
+			}
 
         // Do any additional setup after loading the view.
     }
